3006140d4fec8939515d2e4af7b475004c750aff
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2024-05-26 20:53:01 +0300

    revert to sessions

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "https://nobilis-facts.vercel.app"
 
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      expose: ['Authorization'],
+
       credentials: true
   end
 end
